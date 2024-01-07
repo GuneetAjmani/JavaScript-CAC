2aa1820698f4de3f8aefa0b19b271147568ec8b9
GuneetAjmani <127482777+GuneetAjmani@users.noreply.github.com>
2024-01-07 13:24:07 +0000

    Comparision

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+/*
+avoid these condition check
+{
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+}
+*/
+
+// === strict check => checks both values and datatype
+
+console.log("2" === 2);
