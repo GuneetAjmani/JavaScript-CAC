4c56e693e67b5a9671e04810169557d800162617
GuneetAjmani <127482777+GuneetAjmani@users.noreply.github.com>
2024-01-07 10:05:40 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //traet all JS Code as newer version
+
+// alert("Hello") // we are using node js not browser
+
+let name = "Guneet"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => uninitalised value
+// symbol => unique
+
+// object
+
+console.log(typeof "Guneet");
+
+console.log(typeof age);
+
+console.log(typeof null); // object
+
+console.log(typeof undefined); // undefined
